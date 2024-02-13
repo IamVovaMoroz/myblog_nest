2b49cf496fe5499d54f45ed89e39340185f7d13d
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-02-13 10:38:36 +0100

    ready

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService {
 	findAll(): string[] {
-		return ["dragons", "coffee", "services", "dom", "services"]
+		return ["dragons", "coffee", "services", "dom", "services1"]
 	}
 
 }
