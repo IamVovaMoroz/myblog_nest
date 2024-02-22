816da6f72bce81885c346e3622e4133a5e9a096f
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-02-22 12:33:06 +0100

    ok

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { TagService } from './tag.service';
-// ---
+
 @Controller("tags")
 export class TagController {
 	constructor(private readonly tagService: TagService) {
